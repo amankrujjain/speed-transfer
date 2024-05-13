1eb3574ac5dbb5b97c3c4c7e34889e0f291ab7b5
Aman kumar ujjain <amankrujjain@gmail.com>
2024-05-13 20:09:35 +0530

    env removed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,22 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn ={firstName:'Aman'}
   return (
-    <div>Home</div>
+    <section className='Home'>
+      <div className="home-content">
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiency"
+            
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
